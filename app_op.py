d52a9ddcb986c8634b117a493f6dde76475a6998
luan <luan640@gmail.com>
2023-06-19 15:23:01 -0300

    aproveitamento

--- app_op.py
+++ app_op.py
@@ -245,9 +245,10 @@ def page2():
 
             # caract_op['Aproveitamento'] = pd.to_numeric(caract_op['Aproveitamento'], errors='coerce')
             # caract_op['qt. chapa'] = pd.to_numeric(caract_op['qt. chapa'], errors='coerce')
-            
+
+            table['Aproveitamento'] = table['Aproveitamento'].astype(str)            
             table['Aproveitamento'] = table['Aproveitamento'].replace(",00","", regex=True).replace("%","", regex=True).replace("\.","",regex=True)
-            
+
             for i in range(len(table)):
                 if table['Aproveitamento'][i][0] != '0':
                     table['Aproveitamento'][i] = '0.' + table['Aproveitamento'][i]
