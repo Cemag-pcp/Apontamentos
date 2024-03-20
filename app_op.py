f0962777151cfdefe045022788d78e2c3de8ad56
luan <luan640@gmail.com>
2024-03-20 09:51:50 -0300

    comentando df

--- app_op.py
+++ app_op.py
@@ -89,7 +89,7 @@ def page1():
         
             # Op extraída do pronest
 
-            df = pd.read_excel(r"C:\Users\pcp2\Downloads\OP18486 PL1 #5,32 6000 X 1500.xls")
+            # df = pd.read_excel(r"C:\Users\pcp2\Downloads\OP18486 PL1 #5,32 6000 X 1500.xls")
 
             name_sheet = '1t7Q_gwGVAEwNlwgWpLRVy-QbQo7kQ_l6QTjFjBrbWxE'
             worksheet = 'Criadas'
